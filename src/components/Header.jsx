86855cb634d1964a98922cb4d8026b73219b9785
Matthias <matthias0@skiff.com>
2023-01-04 12:24:43 +0100

    Setup and style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,82 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+import { ReactComponent as BookSVG } from "../icons/book.svg"
+import { ReactComponent as BookmarkSVG } from "../icons/bookmark.svg"
+import { ReactComponent as SearchSVG } from "../icons/search.svg"
 
 const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  padding: 1rem 2rem;
+`
 
+const IconHolder = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.75rem;
+  padding: 1rem;
+`
+
+const Icon = css`
+  stroke: ${({ light }) => (light ? "#f9f9f9" : "#222")};
+  stroke-width: 2px;
+  width: 1.25rem;
+  height: 1.25rem;
+`
+
+const BookIcon = styled(BookSVG)`
+  ${Icon}
+`
+
+const BookmarkIcon = styled(BookmarkSVG)`
+  ${Icon}
+`
+
+const SearchIcon = styled(SearchSVG)`
+  ${Icon}
+`
+
+const Logo = styled.div`
+  ${IconHolder}
+  border-radius: 100%;
+  background-color: #222;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+const Button = styled.button`
+  ${IconHolder}
+  background-color: #f1f1f1;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  border: none;
+
+  &:hover {
+    background-color: #e6e6e6;
+  }
 `
 
 const HeaderComponent = () => (
-    <Header></Header>
+  <Header>
+    <Logo>
+      <BookIcon light />
+    </Logo>
+    <ButtonGroup>
+      <Button>
+        <BookmarkIcon />
+      </Button>
+      <Button>
+        <SearchIcon />
+      </Button>
+    </ButtonGroup>
+  </Header>
 )
 
 export default HeaderComponent
