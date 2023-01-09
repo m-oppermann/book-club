7123de7035f044ab33ce5465b2d165a53f751794
Matthias <matthias0@skiff.com>
2023-01-09 14:33:01 +0100

    Refine DetailPanel component

--- src/components/DetailPanel.jsx
+++ src/components/DetailPanel.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { Cover, Image, Title, Author } from "./Book.jsx"
 import { Icon, Button } from "./Header.jsx"
@@ -15,8 +15,9 @@ const Panel = styled.article`
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 2;
-  width: 40vw;
-  padding: 3.5rem;
+  width: 50rem;
+  height: 22.75rem;
+  padding: 3rem;
   border-radius: 2.5rem;
   background: var(--color);
   box-shadow: 0 0 64px rgba(0, 0, 0, 0.1);
@@ -46,7 +47,7 @@ const ButtonLarge = styled(Button)`
   justify-content: center;
   align-items: center;
   gap: 0.5rem;
-  border-radius: 2rem;
+  border-radius: 1.75rem;
   font-size: 1rem;
   width: 100%;
   background: var(--color-contrary);
@@ -55,9 +56,9 @@ const ButtonLarge = styled(Button)`
   &:hover {
     background: var(--color-contrary-1);
 
-      @media (prefers-color-scheme: dark) {
-        background: var(--color-contrary-2);
-      }
+    @media (prefers-color-scheme: dark) {
+      background: var(--color-contrary-2);
+    }
   }
 `
 
@@ -71,14 +72,34 @@ const About = styled.figcaption`
   display: flex;
   flex-direction: column;
   alight-items: flex-start;
-  flex: 3;
   gap: 16px;
+  height: 100%;
+  overflow: scroll;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 3;
+  position: relative;
+  height: 100%;
+`
+
+const ScrollGradient = styled.span`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 4rem;
+  width: 100%;
+  background: linear-gradient(to top, var(--color), transparent);
+  opacity: ${({ scrollPosition }) => scrollPosition === 1 ? 1 : 0};
+  pointer-events: none;
+  transition: opacity 0.5s ease;
 `
 
 const TitleLarge = styled(Title)`
   font-size: 1.75rem;
   cursor: auto;
-  max-width: 90%;
+  max-width: 80%;
 `
 
 const AuthorLarge = styled(Author)`
@@ -86,8 +107,9 @@ const AuthorLarge = styled(Author)`
 `
 
 const Description = styled.p`
-  margin: 1rem 0 0 0;
+  margin: 0.75rem 0 0;
   color: var(--color-3);
+  white-space: pre-wrap;
 `
 
 const Published = styled.p`
@@ -97,30 +119,46 @@ const Published = styled.p`
   color: var(--color-3);
 `
 
-const DetailPanel = ({ book }) => (
-  <Panel>
-    <Holder>
-      <Cover>
-        <Image
-          src={book.cover}
-          alt={`Book cover for ${book.title} by ${book.author}`}
-        />
-      </Cover>
-      <ButtonLarge>
-        {<AddIcon light title="Add icon" />}
-        Add to list
-      </ButtonLarge>
-    </Holder>
-    <About>
-      <TitleLarge>{`${book.title}: ${book.subtitle}`}</TitleLarge>
-      <AuthorLarge>by {book.author}</AuthorLarge>
-      <Description>{book.description}</Description>
-      <Published>Published in {book.published}</Published>
-    </About>
-    <ButtonClose>
-      <CloseIcon />
-    </ButtonClose>
-  </Panel>
-)
+const DetailPanel = ({ book }) => {
+  const [scrollPosition, setScrollPosition] = useState(1)
+
+  const handleScroll = event => {
+    const pixelScrolled = event.target.scrollTop
+    const scrollHeight = event.target.scrollHeight
+    const elementHeight = event.target.clientHeight
+    const hiddenHeight = scrollHeight - elementHeight
+    setScrollPosition((hiddenHeight - pixelScrolled) / hiddenHeight)
+  }
+
+  return (
+    <Panel>
+      <Holder>
+        <Cover>
+          <Image
+            src={book.cover}
+            alt={`Book cover for ${book.title} by ${book.author}`}
+          />
+        </Cover>
+        <ButtonLarge>
+          {<AddIcon light title="Add icon" />}
+          Add to list
+        </ButtonLarge>
+      </Holder>
+      <Wrapper>
+        <About onScroll={handleScroll} onLoad={handleScroll}>
+          <TitleLarge>{`${book.title}: ${book.subtitle}`}</TitleLarge>
+          <AuthorLarge>by {book.author}</AuthorLarge>
+          <Description>{book.description}</Description>
+          <Published>Published in {book.published}</Published>
+        </About>
+        <ScrollGradient scrollPosition={scrollPosition} />
+      </Wrapper>
+
+      <ButtonClose>
+        <CloseIcon />
+      </ButtonClose>
+    </Panel>
+  )
+}
 
 export default DetailPanel
